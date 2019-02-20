c0e9b344175f3d80c5656f13a2a76f9a6bfb85bd
MaferMazu <13-10787@usb.ve>
2019-02-20 16:55:30 -0400

    Edicion

--- main.c
+++ main.c
@@ -21,19 +21,20 @@ int main()
 		printf("● (3) Cifrar mensajes\n");
 		printf("● (4) Mostrar un esquema de cifrado\n");
 		printf("● (5) Borrar un esquema de cifrado debido a un error\n");
-		printf("● (6) SALIR");
+		printf("● (6) SALIR\n\n");
+		printf("Opcion: ");
 
 		scanf("%d",&opcion);
 		switch(opcion){
 			case 1:
 				while(1){
 					
-					char fecha[99];
+					char fecha[12];
 					char cifrado[99];
 					char descifrado[99];
 
 					printf("Registrar nuevos mensajes:\n\n");
-					printf("Fecha del mensaje: ");
+					printf("Fecha del mensaje de la forma DD-MM-AAAA : ");
 					scanf("%s",fecha);
 
 					printf("Mensaje cifrado: ");
