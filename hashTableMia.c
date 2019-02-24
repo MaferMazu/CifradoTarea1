6cf8c7e647d9a13eea7f5c4e56b684277ba7fb21
María Fernanda Magallanes <35668326+MaferMazu@users.noreply.github.com>
2019-02-24 16:49:54 -0400

    Update hashTableMia.c

--- hashTableMia.c
+++ hashTableMia.c
@@ -3,104 +3,141 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define ABC "ABCDEFGHIJKLMNOPQRSTUVWXYZ #@\0"
-#define SIZE 29
+/*
+TABLA DE HASH CON SUS FUNCIONES BASICAS
+*/
 
-// Mi caja con los dos valores.
+#define SIZE 27
+
+//Esta es una caja que tiene dato y clave
 struct DataItem {
-   // valor encriptado
-   char *data;
-   // valor real
-   char *key;
+   int data;   
+   int key;
 };
 
-struct DataItem* hashArray[SIZE]; 
-struct DataItem* dummyItem;
-struct DataItem* item;
 
-int hashCode(char *key) {
-    //Busca la clave en el ABCdario y le pone un indice.
-    int index=(int)(strchr(ABC,key[0])-ABC);
-    return index;   
+//Este es un arreglo de cajas que tiene dato y clave
+//struct DataItem* hashArray[SIZE]; 
+
+//Con esto organizo mis cajas en la tabla de hash
+int hashCode(int key) {
+	if(key==32){
+		return 26;
+	}
+    return (key-65)%SIZE;
 }
 
-struct DataItem *search(char *key) {
-   //Obtengo el indice
-   int hashIndex = hashCode(key);  
-    
-   //Busco en la tabla hasta encontrar keys iguales 
-    if(hashArray[hashIndex]->key[0] == key[0]){
-        return hashArray[hashIndex]; 
-    }
+// Funcion que busca una caja a partir de una clave
+struct DataItem *search(int key,struct DataItem* hashArray) {
+   int hashIndex = hashCode(key);
+
+   while(hashArray[hashIndex] != NULL) {
+	
+      if(hashArray[hashIndex]->key == key)
+         return hashArray[hashIndex]; 
+
+      ++hashIndex;
+
+      hashIndex %= SIZE;
+   }        
+	
    return NULL;        
 }
 
-void insert(char *key,char *data) {
-    // Creo un elemento (caja con dato y clave)
-   struct DataItem *item = (struct DataItem*) malloc(sizeof(struct DataItem));
-   item->data[0] = data[0];  
-   item->key[0] = key[0];
+//Funcion insertar un cajita a partir de sus datos
+void insert(int key,int data,struct DataItem* hashArray) {
 
-   //Obtengo su indice y lo inserto en la tabla de hash
-   // Pd: este insert no resuelve colisiones (como planteamos el ejercicio no hace falta)
+   struct DataItem *item = (struct DataItem*) malloc(sizeof(struct DataItem));
+   item->data = data;  
+   item->key = key;
+
+   
    int hashIndex = hashCode(key);
    hashArray[hashIndex] = item;
 }
 
-struct DataItem* delete(char *key) {
-   int hashIndex = hashCode(key);  
-   if(hashArray[hashIndex]->key[0] == key[0]) {
-      struct DataItem* temp = hashArray[hashIndex];
-      hashArray[hashIndex] = dummyItem; 
-      return temp;
-   }
-        
+//Funcion que elimina un elemento de la tabla de hash
+struct DataItem* delete(struct DataItem* item,struct DataItem* hashArray) {
+   int key = item->key;
+
+ 
+   int hashIndex = hashCode(key);
+
+
+    if(hashArray[hashIndex]->key == key) {
+       struct DataItem* temp = hashArray[hashIndex]; 
+       hashArray[hashIndex] = NULL; 
+       return temp;
+    }
+     
+	
    return NULL;        
 }
 
-void display() {
+//Muestra la tabla de hash
+void display(struct DataItem* hashArray) {
    int i = 0;
-    
+	
    for(i = 0; i<SIZE; i++) {
-    
+	
       if(hashArray[i] != NULL)
-         printf(" [%c:%c]",hashArray[i]->key[0],hashArray[i]->data[0]);
+         printf(" [%c:%c]",hashArray[i]->key,hashArray[i]->data);
       else
          printf("[ : ]");
    }
-    
+	
    printf("\n");
 }
 
+//Crear tabla
+struct DataItem *create_hashTable(int size){
+	struct DataItem* hashArray[size];
+	return hashArray;
+}
+
+/*
+FUNCIONES PARTICULARES PARA LA TAREA
+*/
+
+struct DataItem* createHashTableWithStrings(char *string1,char *string2){
+	struct DataItem* tabla = create_hashTable(27);
+	for(int i=0;i<strlen(string1);i++){
+		if(search(string1[i],tabla)==NULL){
+			printf("%c\n", string1[i]);
+			insert(string1[i],string2[i],tabla);
+		}
+	}
+	return tabla;
+}
+
+char* cifrar(char *string,struct DataItem* tabla){
+	char string2[50];
+	char *stringPtr = string2;
+	for(int i=0;i<strlen(string);i++){
+		if(search(string[i],tabla)!=NULL){
+			string2[i]= (search(string[i],tabla))->data;
+		}
+		else{string2[i]=35;}
+	}
+	return stringPtr;
+}
+
+/*
+MAIN PARA PROBAR LAS FUNCIONES DE HASH
+*/
 int main() {
-   dummyItem = (struct DataItem*) malloc(sizeof(struct DataItem));
-   dummyItem->data = "@";  
-   dummyItem->key = "@"; 
-
-   char *letra1 ="A";
-   char *letra2 ="B";
-   insert(letra1, letra2);
-   insert("C", "A");
-   insert("B", "C");
-   insert("H", "O");
-   insert("O", "H");
-   insert("L", "U");
-   
-   display();
-   item = search("A");
-
-   if(item != NULL) {
-      printf("Element found: %s\n", item->data);
-   } else {
-      printf("Element not found\n");
-   }
-   
-   delete("E");
-   item = search("H");
-
-   if(item != NULL) {
-      printf("Element found: %s\n", item->data);
-   } else {
-      printf("Element not found\n");
-   }
+   struct DataItem *pedro;
+   char string[50];
+   char string1[50];
+   char string2[50];
+   printf("%s\n","String1: ");
+   fgets(string1, sizeof(string1), stdin);
+   printf("%s\n","String2: ");
+   fgets(string2, sizeof(string2), stdin);
+   pedro=createHashTableWithStrings(string1,string2);
+   display(pedro);
+   printf("%s\n","StringACifrar: ");
+   fgets(string, sizeof(string), stdin);
+   char *p = cifrar(string,pedro);
+   printf("%s\n", p);
 }
